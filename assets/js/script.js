24a2d06d13e8af2265e9f13ddf83fa951f3aee01
Katia Laws <knadia@live.com>
2024-11-11 10:04:34 +0000

    Add displayException function

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -57,6 +58,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -95,4 +97,14 @@ function displayStatus(data) {
 
 }
 
-console.log('If you see this message, your script is running.');
+function displayException(data) {
+    let heading = `An Exception Occurred`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
